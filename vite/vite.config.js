efb0929912265522918dc6a9cf13325e1f22e0c7
Semyon Rozhkov <semro@outlook.com>
2023-07-06 13:48:55 +0300

    perf: inline critical css

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import PluginCritical from "rollup-plugin-critical";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    PluginCritical.default({
+      criticalUrl: "https://semro.github.io/shri2023-task/",
+      criticalBase: "../",
+      criticalPages: [{ uri: "", template: "index" }],
+      criticalConfig: {
+        inline: true,
+      },
+    }),
+  ],
   build: {
     outDir: "../",
   },
