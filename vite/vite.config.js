3abd1c33f53b3f01b32a783fdc15f014e89dddd7
Semyon Rozhkov <semro@outlook.com>
2023-07-09 12:01:35 +0300

    perf: optimize `svg` images

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import PluginCritical from "rollup-plugin-critical";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    ViteImageOptimizer(),
     PluginCritical.default({
       criticalUrl: "../index.html",
       criticalBase: "../",
